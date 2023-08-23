a7c56faa38c992e7b95b2dd597c60550c46e235a
Tomás <t.diaz.soto@gmail.com>
2023-08-23 13:28:18 -0400

    deploy

--- app/page.js
+++ app/page.js
@@ -2,13 +2,14 @@ const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}
 const getCatImage = async () => {
   const response = await fetch(url,{ cache: "no-store" })
   const { data } = await response.json()
-  return data?.images?.original?.url
+  return data.images.original.url
 }
 import CatImage from "./image/page"
 
 export default async function Home () {
   const image = await getCatImage()
   console.log(image)
+  console.log(process.env.API_KEY)
 
   return (
     <main className="flex min-h-screen p-28  flex-col items-center">
